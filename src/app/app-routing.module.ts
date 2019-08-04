a410e951b859c6228f78de37818b482c900417b6
Robert San <robertsandru96@gmail.com>
2019-08-04 15:11:02 +0300

    Note components - detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentWrapperComponent } from './reusable/content-wrapper/content-wrapper.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentWrapperComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftBarComponent } from './reusable/left-bar/left-bar.component';
+import { TopBarComponent } from './reusable/top-bar/top-bar.component';
+import { ContentWrapperComponent } from './reusable/content-wrapper/content-wrapper.component';
+import { NotesComponent } from './notes/notes.component';
+import { NoteDetailComponent } from './notes/note-detail/note-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftBarComponent,
+    TopBarComponent,
+    ContentWrapperComponent,
+    NotesComponent,
+    NoteDetailComponent
   ],
   imports: [
     BrowserModule,
